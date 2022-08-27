cf0b1eafb28c3ca1775ce8e899ce9556cd988c70
code-yy <jiyeyouren523@gmail.com>
2022-08-27 13:49:03 +0900

    :art: テーブルに変更、非同期処理の部分でローディングを表示させるように

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ import { AuthProvider } from "src/components/ui-libraries/AuthProvider";
 import { AuthModal } from "src/components/feature/AuthModal";
 import { TECH_UNI } from "src/components/utils/constants/tokens";
 import { LINKS } from "src/components/utils/constants/link";
+import { Layout } from "src/components/layout";
 
 const App = ({ Component, pageProps, router }: AppProps): JSX.Element => {
   return (
@@ -35,7 +36,9 @@ const AppPage: FC<AppProps> = ({ Component, pageProps, router }) => {
 
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </AuthProvider>
   );
 };
