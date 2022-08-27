42ddf58fe190d7ee7578700fa3947d8462a2f3bf
code-yy <jiyeyouren523@gmail.com>
2022-08-27 15:25:59 +0900

    :recycle: リファクタして実際に使用。ついでに軽くメンバー側のリファクタ

--- src/components/page/Member/index.tsx
+++ src/components/page/Member/index.tsx
@@ -1,68 +1,70 @@
-import { collection, getDocs } from "firebase/firestore";
-import { useState, useEffect, FC, Suspense } from "react";
+import { useEffect, FC } from "react";
 import { Text } from "@mantine/core";
 import { ComitteeCard, MemberCard } from "src/components/feature/Member/MemberCard";
 import { Layout } from "src/components/layout";
 import { AppLoading } from "src/components/ui-libraries/AppLoading";
-import { db } from "src/components/utils/libs/firebase";
 import { useCurrentUser } from "src/global-states/atoms";
-import { User } from "src/components/utils/libs/firebase/index";
+import { useFetchMembers } from "src/hooks/user/useFetchUserList";
+import { User } from "src/modules/user/user.entity";
 
 export const Member: FC = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
   const { currentUser } = useCurrentUser();
-  const myField = currentUser?.field;
+  const { fetchUser, userList, isLoading } = useFetchMembers();
 
   useEffect(() => {
-    try {
-      const getUsers = async () => {
-        const colRef = collection(db, "users");
-        const users = await getDocs(colRef);
-        setUsers(users.docs.map((doc) => doc.data() as User));
-      };
-      getUsers();
-    } finally {
-      setIsLoading(false);
-    }
-  }, [isLoading]);
+    fetchUser();
+  }, []);
 
-  if (!currentUser) return null;
-  const myFieldMembers = users.filter((user) => user.field === myField && user.uid !== currentUser.uid);
+  if (isLoading || userList == null || currentUser == null) return <AppLoading />;
+
+  const myField = currentUser?.field;
+  const displayMyFieldMembers: () => JSX.Element | JSX.Element[] = () => {
+    const myFieldMembers: User[] = userList.filter((user) => {
+      const isMyFieldMember: boolean = user.field === myField;
+      const isNotCurrentUser: boolean = user.uid !== currentUser.uid;
+      return isMyFieldMember && isNotCurrentUser;
+    });
+
+    if (myFieldMembers.length === 0) return <Text>いないようです😭</Text>;
+    return myFieldMembers.map((myFieldMember) => {
+      return <MemberCard key={myFieldMember.uid} data={myFieldMember} />;
+    });
+  };
+
   return (
     <Layout>
-      <Suspense fallback={<AppLoading />}></Suspense>
       <div className="flex w-full flex-col flex-wrap gap-5">
         <div className="pt-5">
           <Text weight="bold">コミッティー</Text>
           <div className="flex flex-wrap gap-5">
-            {users
+            {userList
               .filter((user) => {
                 const isDistinctLeaderPosition = user.position === 3 || user.position === 4 || user.position === 5;
                 return isDistinctLeaderPosition;
               })
-              .map((user, index) => (
-                <ComitteeCard key={index} {...user} />
+              .map((user) => (
+                <ComitteeCard key={user.uid} {...user} />
               ))}
           </div>
         </div>
         <div>
           <Text weight="bold">{myField}を専門としているメンバー</Text>
-          <div className="flex gap-2 rounded-md bg-white py-6 px-4 shadow-md">
-            {myFieldMembers.length === 0 && <Text>いないようです😭</Text>}
-            {myFieldMembers.map((user) => {
-              return <MemberCard key={user.displayName} data={user} />;
-            })}
-          </div>
+          <div className="flex gap-2 rounded-md bg-white py-6 px-4 shadow-md">{displayMyFieldMembers()}</div>
         </div>
-
         <Text weight="bold">全員</Text>
         <div className="flex gap-2 rounded-md bg-white py-6 px-4 shadow-md">
-          {users.map((user, index) => {
-            return <MemberCard data={user} key={index} />;
+          {userList.map((user) => {
+            return <MemberCard data={user} key={user.uid} />;
           })}
         </div>
       </div>
     </Layout>
   );
 };
+
+// TODO: リファクタ 📝
+// 専門メンバーと全員でコンポーネント共通化できそう
+// コミッティーでもできるかも？？
+
+// TODO: 実装メモ 🚧
+// myFieldを設定していない場合どうするのか？？
