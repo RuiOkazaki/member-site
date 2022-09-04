a58355c61b127b83ac9add27a03ed784602a5698
Shin.Yamamoto <yamashin0123@outlook.jp>
2022-09-04 11:38:35 +0900

    🐛 fix review

--- src/components/feature/UnderConstruction.tsx
+++ src/components/feature/UnderConstruction.tsx
@@ -1,13 +1,19 @@
 import { Text } from "@mantine/core";
 import Image from "next/image";
+import { FC } from "react";
 
-export const UnderConstruction = () => {
+export const UnderConstruction: FC = () => {
   return (
     <div className="flex h-full flex-col items-center justify-center text-center">
       <Text size="xl" weight="bold">
         開発中です🛠
       </Text>
-      <Image src={"/other/under_construction.svg"} alt="" width={700} height={430} />
+      <Image
+        src={"/other/under_construction.svg"}
+        alt="現在このページは使用することができません。"
+        width={700}
+        height={430}
+      />
     </div>
   );
 };
