d5b5d9328b1e0588564282e82951eb1126c17007
Shin.Yamamoto <yamashin0123@outlook.jp>
2022-09-06 03:53:03 +0900

    ✅ 勉強会詳細ページ

--- src/components/feature/Member/MemberProfileIcon.tsx
+++ src/components/feature/Member/MemberProfileIcon.tsx
@@ -1,16 +1,19 @@
 /* eslint-disable @next/next/no-img-element */
+import { Avatar, Text } from "@mantine/core";
 import { FC } from "react";
 
-type ProfileImgProps = {
+type Props = {
   displayName: string | undefined;
   photoURL: string | undefined;
 };
 
-export const MemberProfileIcon: FC<ProfileImgProps> = ({ displayName, photoURL }) => {
+export const MemberProfileIcon: FC<Props> = ({ displayName, photoURL }) => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <img src={photoURL} alt={`${displayName}の画像`} className="h-12 w-12  rounded-full" />
-      <p className="w-20  truncate pt-1 text-center text-xs">{displayName}</p>
+      <Avatar src={photoURL} radius="xl" size={44} className="hover:opacity-80" alt="ゲスト" />
+      <Text size="sm" className="w-20  truncate pt-1 text-center">
+        {displayName ?? "ゲスト"}
+      </Text>
     </div>
   );
 };
--- src/components/page/StudyMeeting/StudyMeetingDetail.tsx
+++ src/components/page/StudyMeeting/StudyMeetingDetail.tsx
@@ -1,45 +1,102 @@
+import { Text } from "@mantine/core";
 import { useRouter } from "next/router";
 import { FC, useEffect, useState } from "react";
+import { FieldInterest } from "src/components/feature/Member/FieldInterest";
+import { MemberProfileIcon } from "src/components/feature/Member/MemberProfileIcon";
+import { AppLink } from "src/components/ui-libraries/AppLink";
 import { AppLoading } from "src/components/ui-libraries/AppLoading";
 import { ChevronLeftIcon } from "src/components/ui-libraries/icon";
 import { useFetchEventList } from "src/hooks/event/useFetchEventList";
 import { useConvertTimeStamp } from "src/hooks/useConvertTimeStamp";
+import { useFetchUserList } from "src/hooks/user/useFetchUserList";
 import { Event } from "src/modules/event";
+import { User } from "src/modules/user";
 
 export const StudyMeetingDetail: FC = () => {
   const router = useRouter();
+  const eventTitle = router.query.title;
+
   const { fetchEventList, eventList, isLoading } = useFetchEventList();
+  const { fetchUser, userList } = useFetchUserList();
+
   const [thisEvent, setThisEvent] = useState<Event>();
   const { year, monthAndDate, time } = useConvertTimeStamp({ date: thisEvent?.date });
 
   useEffect(() => {
     fetchEventList();
-    const eventTitle = router.query.title;
-    const event = eventList?.find((event: Event) => event.title === eventTitle);
-    setThisEvent(event);
-  }, []);
+    fetchUser();
+
+    // todo recoilで管理した方がいいかもしれない
+    const setThisEventFunc = () => {
+      const thisEvent = eventList?.find((event) => event.title === eventTitle);
+      setThisEvent(thisEvent);
+    };
+    setThisEventFunc();
+  }, [eventList?.length]);
 
   const handleBackClick = () => {
     router.back();
   };
 
+  if (!thisEvent) return null;
   if (isLoading || eventList == null) return <AppLoading />;
 
   return (
-    <>
+    <div className="mb-10">
       <button className="relative top-0 left-0 cursor-pointer py-2" onClick={handleBackClick}>
         <ChevronLeftIcon />
-
-        <div className="flex flex-col gap-2">
-          <div className="text-2xl font-bold">{thisEvent?.title}</div>
-          <div>
-            {year}
-            {monthAndDate}
-            {time}
-          </div>
-          <div className="text-lg">{thisEvent?.field}</div>
-        </div>
       </button>
-    </>
+
+      <div className="flex flex-col items-center justify-center gap-8">
+        <div>
+          <div>
+            {thisEvent.photoUrl !== null && (
+              <img src={thisEvent.photoUrl} alt={thisEvent.title} className="rounded-lg" />
+            )}
+          </div>
+
+          <Text size="xl" weight="bold">
+            {thisEvent?.title}
+          </Text>
+
+          <div className="flex gap-8">
+            <Text size="sm" weight="bold" color="gray">
+              {year}.{monthAndDate}.{time}
+            </Text>
+            <FieldInterest field={thisEvent?.field} />
+          </div>
+        </div>
+
+        <div className="w-full rounded-md bg-gray-100 p-4">
+          <Text size="lg" weight="bold">
+            登壇者
+          </Text>
+          {thisEvent.materials.map((material, i) => {
+            const presenter = userList?.find((user: User) => user.uid === material.presenterUuid);
+            return (
+              <div key={i} className={`py-6  ${i === thisEvent.materials.length - 1 ? "" : "border-b"} `}>
+                <div className="flex">
+                  <MemberProfileIcon displayName={presenter?.displayName} photoURL={presenter?.photoURL} />
+                  <Text>{material.title}</Text>
+                </div>
+                <AppLink href={material.content}>{material.content}</AppLink>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="w-full rounded-md bg-gray-100 p-4">
+          <Text size="lg" weight="bold">
+            参加者
+          </Text>
+          <div className="flex">
+            {thisEvent.participantsUuid.map((participantUud, i) => {
+              const user = userList?.find((user: User) => user.uid === participantUud);
+              return <MemberProfileIcon key={i} displayName={user?.displayName} photoURL={user?.photoURL} />;
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
